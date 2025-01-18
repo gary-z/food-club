4caf80a2d2a18e7ebddb323d8dd7d9093424d302
Gary <gary-z@users.noreply.github.com>
2025-01-18 14:01:36 -0800

    Organize

--- simple_win_rate_model.py
+++ simple_win_rate_model.py
@@ -4,19 +4,29 @@ from pirates import pirates, Pirate, courses
 from collections import defaultdict, namedtuple
 from typing import List
 
-DAYS = 50000
+DAYS = 150000
 
-wins = defaultdict(int)
-FOOD_MULTIPLIER = 2
+
+def get_simulation_score():
+    pirates_mine = list(pirates)
+    wins = defaultdict(int)
+    for _ in range(DAYS):
+        random.shuffle(pirates_mine)
+        groups = [pirates_mine[i : i + 4] for i in range(0, len(pirates_mine), 4)]
+        for group in groups:
+            winner = get_group_winner(group)
+            wins[winner.name] += 1
+
+    actual_win_rates = [pirate.win_rate for pirate in pirates]
+    simulated_win_rates = [wins[pirate.name] / DAYS for pirate in pirates]
+
+    return average_log_ratio_difference(simulated_win_rates, actual_win_rates)
 
 
 def get_group_winner(group: List[Pirate]):
     cs = random.sample(courses, 10)
     winner = max(group, key=lambda pirate: get_pirate_score(pirate, cs))
     return winner
-
-
-deviation = 20
 
 
 def get_pirate_score(pirate: Pirate, cs: List[str]):
@@ -26,21 +36,14 @@ def get_pirate_score(pirate: Pirate, cs: List[str]):
         is_allergy = course in pirate.allergy_courses
 
         if is_fav and not is_allergy:
-            score += (135 - pirate.strength) * random.random()
+            score += (
+                125 - pirate.strength * 0.8 - pirate.weight * 0.1
+            ) * random.random()
         elif is_allergy and not is_fav:
-            score += (175 - pirate.strength) * random.random()
+            score += (170 - pirate.strength) * random.random()
         else:
             score += (155 - pirate.strength) * random.random()
     return -score
-
-
-pirates_mine = list(pirates)
-for _ in range(DAYS):
-    random.shuffle(pirates_mine)
-    groups = [pirates_mine[i : i + 4] for i in range(0, len(pirates_mine), 4)]
-    for group in groups:
-        winner = get_group_winner(group)
-        wins[winner.name] += 1
 
 
 def average_log_ratio_difference(simulated, historical):
@@ -60,23 +63,4 @@ def average_log_ratio_difference(simulated, historical):
     return total_log_diff / count
 
 
-actual_win_rates = [pirate.win_rate for pirate in pirates]
-simulated_win_rates = [wins[pirate.name] / DAYS for pirate in pirates]
-
-
-for pirate in sorted(pirates, key=lambda p: p.win_rate):
-    print(
-        "%.2f\t%.2f\t%d\t%d\t%s"
-        % (
-            pirate.win_rate,
-            wins[pirate.name] / DAYS,
-            len(pirate.favorite_courses),
-            len(pirate.allergy_courses),
-            pirate.name,
-        )
-    )
-
-print(
-    "Log ratio difference %.3f"
-    % average_log_ratio_difference(simulated_win_rates, actual_win_rates)
-)
+print("Log ratio difference %.3f" % get_simulation_score())
